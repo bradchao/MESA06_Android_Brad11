85f96825843fb2015240911ab377f9548fdd79dd
Brad Chao <brad@brad.tw>
2016-11-21 14:56:07 +0800

    v4

--- app/src/main/java/com/example/user/brad11/MainActivity.java
+++ app/src/main/java/com/example/user/brad11/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.user.brad11;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
+    private MyDBHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.tv);
 
+        dbHelper = new MyDBHelper(this, "iii", null, 1);
+        db = dbHelper.getReadableDatabase();
 
     }
+
+    public void query(View v){
+        // SELECT * FROM cust
+        // db.execSQL("SELECT * FROM cust");
+        Cursor cursor = db.query("cust",null,null,null,null,null,null);
+
+    }
+
+
 }
