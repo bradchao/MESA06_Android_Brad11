869bf53854cbf2ad00764d39c892283f798699ab
Brad Chao <brad@brad.tw>
2016-11-21 14:26:47 +0800

    v3

--- app/src/main/java/com/example/user/brad11/MyDBHelper.java
+++ app/src/main/java/com/example/user/brad11/MyDBHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MyDBHelper extends SQLiteOpenHelper {
+    private String createTable =
+            "CREATE TABLE cust (id INTEGER PRIMARY KEY AUTOINCREMENT,cname TEXT,birthday DATE,tel TEXT)";
     public MyDBHelper(Context context, String name,
                       SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -12,7 +14,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(createTable);
     }
 
     @Override
